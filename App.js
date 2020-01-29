3eac9c76ca06fbda2160100beb8420c857491a7e
mac <mac@esco-3.local>
2020-01-29 03:07:07 +0000

    initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native';
+import Icon from "react-native-vector-icons/FontAwesome";
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+
+        <Text> Ecommerce UI </Text>
+        <Icon name="bars" size={100} />
+      </View>
+    )
+  }
+}
