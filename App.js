ae7c5c2d4fcf90e85591640c7acfb0280fcd6b36
mac <mac@esco-3.local>
2020-01-29 04:16:53 +0000

    update

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native';
-import Icon from "react-native-vector-icons/FontAwesome";
+import Icon from "react-native-vector-icons/FontAwesome"
 export default class App extends Component {
   render() {
     return (
       <View>
-
-        <Text> Ecommerce UI </Text>
+        <Text> Ecommerce App </Text>
         <Icon name="bars" size={100} />
       </View>
     )
