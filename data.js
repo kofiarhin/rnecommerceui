dcb25915a62b1a945b068362bcc2149317b48b63
mac <mac@esco-3.local>
2020-02-10 12:40:26 +0000

    cleared cart code

--- data.js
+++ data.js
@@ -71,7 +71,7 @@ const data = [
 
 
     {
-        id: 1,
+        id: 8,
         name: "iPhone 11 Pro Max",
         cover: "https://www.theiphonewiki.com/w/images/thumb/0/0c/IPhone_11_Pro_Max.png/150px-IPhone_11_Pro_Max.png",
         price: "1200",
@@ -81,7 +81,7 @@ const data = [
 
 
     {
-        id: 1,
+        id: 9,
         name: "iPhone 11",
         cover: "https://www.theiphonewiki.com/w/images/thumb/4/47/IPhone_11.png/150px-IPhone_11.png",
         price: "800",
--- src/components/screens/cart.js
+++ src/components/screens/cart.js
@@ -1,103 +1,12 @@
 import React, { Component } from 'react'
-import { Text, View, Button, FlatList, Image } from 'react-native';
-import { connect } from "react-redux";
-import { clearCart, getCart, storeItem } from "../../../actions";
-import FontAwesome from "react-native-vector-icons/FontAwesome";
+import { Text, View } from 'react-native'
 
-class Cart extends Component {
-
-    componentDidMount() {
-        this.props.dispatch(getCart());
-        console.log('render page')
-    }
-
-
-
-    handleClear = () => {
-
-        //cleart the cart
-        this.props.dispatch(clearCart());
-
-        //get the cart from session storage
-        this.props.dispatch(getCart())
-
-        //redirect to home screen
-        this.props.navigation.navigate("Home")
-
-
-    }
-
-
-
-    renderCart = data => {
-
-        if (data) {
-            let cart = JSON.parse(data);
-
-            return <FlatList
-
-                data={cart}
-                keyExtractor={(item, index) => index}
-
-                renderItem={({ item, index }) => {
-
-                    return <View>
-                        <Text> {item.name} </Text>
-                        <Image source={{ uri: item.cover }} style={{
-                            width: 200,
-                            height: 200
-                        }} />
-                        <FontAwesome name="trash" size={30} color="red" onPress={() => this.handleDelete(item, index)} />
-                    </View>
-                }}
-
-
-            />
-        } else {
-
-            return <View>
-
-                <Text> your cart is empty </Text>
-            </View>
-        }
-    }
-
-    handleDelete = (item, index) => {
-
-        this.props.dispatch(getCart());
-
-        let data = JSON.parse(this.props.cartData.cartData);
-
-        data.splice(index, 1);
-        this.props.dispatch(storeItem(data));
-        this.props.dispatch(getCart());
-
-        console.log("item removed")
-
-
-
-    }
-
+export default class cart extends Component {
     render() {
-
-        // console.log(this.props);
         return (
             <View>
-
-                {this.renderCart(this.props.cartData.cartData)}
-
-                <Button title="Clear Cart" onPress={() => this.handleClear()} />
-                <Button title="Go Back Home" onPress={() => this.props.navigation.navigate("Home")} />
+                <Text> textInComponent </Text>
             </View>
         )
     }
 }
-
-
-const mapStateToProps = state => {
-    return {
-
-        cartData: state.cart
-    }
-}
-export default connect(mapStateToProps)(Cart)
